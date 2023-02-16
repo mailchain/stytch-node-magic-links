2c7963f79ad5b364f57c5af279c83576620f5bea
Antonio Ivanovski <toteto@users.noreply.github.com>
2023-02-17 00:08:19 +0100

    add mailchain magic link

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const url = require('url');
 const stytch = require("stytch")
+const { Mailchain } = require('@mailchain/sdk')
 
 require('dotenv').config()
 
@@ -25,6 +26,7 @@ const client = new stytch.Client({
         env: stytch.envs.test,
     }
 );
+const mailchain = Mailchain.fromSecretRecoveryPhrase(process.env.SECRET_RECOVERY_PHRASE)
 
 // define the homepage route
 app.get("/", (req, res) => {
@@ -33,30 +35,72 @@ app.get("/", (req, res) => {
 
 // takes the email entered on the homepage and hits the stytch
 // loginOrCreateUser endpoint to send the user a magic link
-app.post('/login_or_create_user', function (req, res) {
+app.post('/login_or_create_user', async function (req, res) {
     const params = {
         email: req.body.email,
         login_magic_link_url: magicLinkUrl,
         signup_magic_link_url: magicLinkUrl,
     };
+
+	const isMailchain = params.email.endsWith('mailchain.com');
+	if (isMailchain) {
+		const userSearchResult = await client.users.search({
+		    query: {
+		      operator: "AND",
+		      operands: [
+		        { filter_name: "email_address", filter_value: [ params.email ] },
+		      ],
+		    },
+		  });
+	
+		const existingUser = userSearchResult.results[0];
+		const userToAuth = existingUser ?? (await client.users.create({ create_user_as_pending: true, email: req.body.email,}));	
+		const magicLinkParams = await client.magicLinks.create({ user_id: userToAuth.user_id })
+		const subject = "Stytch Magic 🪄 Link"
+		const magicLinkUrlWithToken = `${magicLinkUrl}?userId=${magicLinkParams.user_id}&token=${magicLinkParams.token}` 
+		const content = {
+			text: `Navigate to ${magicLinkUrlWithToken} to authenticate`,
+			html: `
+			  <div>
+			  	<h1>Your ${existingUser ? 'login' : 'sign up'} request. Click on the button to proceed.</h1>
+				<div style="text-decoration: none; margin:10px; padding:10px; display:inline-block; background-color: rgb(16, 110, 233); border: 1px solid rgb(16, 110, 233); border-radius: 4px;">
+				  <a style="color: white" href={${magicLinkUrlWithToken}}>${existingUser ? 'LOGIN' : 'SING UP'}</a>
+				</div>
+			  </div>`
+		}
+
+		return mailchain.sendMail({
+			from: (await mailchain.user()).address,
+			to: [params.email],
+			subject,
+			content
+		})
+		.then(r => res.render('emailSent'))
+		.catch((e) => {
+			console.error('Failed sending message', e);
+			return res.status(500).render('loginOrSignUp');
+		})
+	}
+
     client.magicLinks.email.loginOrCreate(params)
         .then(
-            // on success, render the emailSent page
-            res.render('emailSent')
-        )
-        .catch(err => {
-            // on failure, log the error then render the homepage
-            console.log(err)
-            res.render('loginOrSignUp')
-        });
-})
-
+		        // on success, render the emailSent page
+		        res.render('emailSent')
+		    )
+		    .catch(err => {
+			        // on failure, log the error then render the homepage
+			        console.log(err)
+			        res.render('loginOrSignUp')
+			    });
+		})
+		
+		
 // This is the endpoint the link in the magic link hits. It takes the token from the
 // link's query params and hits the stytch authenticate endpoint to verify the token is valid
 app.get('/authenticate', function (req, res) {
     const queryObject = url.parse(req.url,true).query;
     client.magicLinks.authenticate(queryObject.token)
-        .then(
+        .then(r =>
             // on success render the logged in view
             res.render('loggedIn')
         )
